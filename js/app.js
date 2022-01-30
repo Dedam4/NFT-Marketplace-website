7dba96221c6da9d4594ab09841d4de4b3a3ea18c
Dedam4 <shtrak2014@mail.ru>
2022-01-30 14:18:06 +0700

    Filter for block Populer

--- js/app.js
+++ js/app.js
@@ -7,73 +7,85 @@ let accordion = document.querySelectorAll('.accordion')
 let accordionHeader = document.querySelectorAll('.accordion__header')
 let accordionItem = document.querySelectorAll('.accordion__item');
 
+let populerLinks = document.querySelectorAll('.populer__link');
+let itemPopuler = document.querySelectorAll('.item_populer');
 
-    
+
+/*Filter for populer block*/ 
+populerLinks.forEach(item => {
 
 
-let index = 0;
-questionsItems.forEach( item =>{
- 
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+        let filterClass = e.target.dataset['f'];
+        itemPopuler.forEach(index => {
+            index.classList.remove('hide');
+            if(!index.classList.contains(filterClass) && filterClass != 'all'){
+                index.classList.add('hide');
+            }
+
+
+        });
+    });
+});
+
+
+
+
+
+questionsItems.forEach(item => {
+
     item.addEventListener('click', () => {
         clearActiveSlasses();
         item.classList.toggle('active');
 
-      
+
     });
 
 });
-function clearActiveSlasses()
-{
-    questionsItems.forEach((item) =>{
+function clearActiveSlasses() {
+    questionsItems.forEach((item) => {
         item.classList.remove('active');
     })
-         
+
 }
-function changeAccordionItems(item){
-    
-    if(item === questionsItems[1]) {
-      
+function changeAccordionItems(item) {
+
+    if (item === questionsItems[1]) {
+
 
 
     }
 
 }
 
+accordionItem.forEach(item => {
 
-
-
-accordionItem.forEach( item =>{
- 
     item.addEventListener('click', () => {
-      
-            item.classList.toggle('active');
 
-     
-       
-       
-       
+        item.classList.toggle('active');
+
 
     });
 
 });
-accordionHeader.forEach( item =>{
- 
+accordionHeader.forEach(item => {
+
     item.addEventListener('click', () => {
         item.classList.toggle('active');
-        
-       
+
 
     });
 
 });
 
-
+/*Burger menu*/ 
 burger.addEventListener('click', event => {
     event.preventDefault();
-    
+
     burger.classList.toggle('active');
     menuActive.classList.toggle('active')
 
-    
-    
+
+
 });
